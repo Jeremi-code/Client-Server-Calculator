ddbda0db0b5604403bfd018db18374ab2627a70e
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-03 13:33:56 -0700

    add multiplication and divison operation

--- Client.js
+++ Client.js
@@ -2,14 +2,14 @@ const { send } = require('process')
 const socketIOClient = require('socket.io-client')
 const serverURL = 'http://localhost:3000'
 const socket = socketIOClient(serverURL)
+
 socket.on('connect', () => {
     console.log('connected to server')
-    socket.emit('message', 'Hello from client')
     sendMessage()
 })
+
 socket.on('messageFromServer', (message) => {
     console.log(message)
-    sendMessage()
 })
 
 function sendMessage(){
@@ -18,16 +18,23 @@ function sendMessage(){
         output: process.stdout
     })
     readline.question('Enter the numbers to add: ', (numbers) => {
+        
         const numArray = numbers.split(' ')
+        if(numbers === 'exit'){
+            readline.close()
+            socket.disconnect()
+            return
+        }
         if(numArray.length !== 2 || isNaN(numArray[0]) || isNaN(numArray[1])){
             console.log('Invalid input')
             readline.close()
-            sendMessage()
+            return
         }
         const a = parseInt(numArray[0])
         const b = parseInt(numArray[1])
         socket.emit('messageFromClient', a, b)
-})
+        sendMessage()
+        })
 }
 
 socket.on('disconnect', () => {
