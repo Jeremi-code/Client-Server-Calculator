1b895f971d754f829f88ab081efb80a22457bd04
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-03 12:24:33 -0700

    initial commit for server side code

---
+++ Server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require("http");
+const SocketIO = require("socket.io");
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("messageFromClient", (a, b) => {
+      const sum = addNumbers(a, b);
+      console.log(`Sum of ${a} and ${b} is ${sum}`);
+      socket.emit("messageFromServer", `Sum of ${a} and ${b} is ${sum}`);
+  });  
+});
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
