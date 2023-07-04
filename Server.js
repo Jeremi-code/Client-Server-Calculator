bd109e52a622dd7bcc9a3005b306e8c97cabcb7c
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-04 01:17:14 -0700

    clean the code

--- Server.js
+++ Server.js
@@ -10,18 +10,39 @@ io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("messageFromClient", (a, b) => {
       const sum = addNumbers(a, b);
-      console.log(`Sum of ${a} and ${b} is ${sum}`);
+      const difference = subtractNumbers(a, b);
+      const product = multiplyNumbers(a, b);
+      const quotient = divideNumbers(a, b);
+      console.log(`Server received message: ${a} and ${b}`);
       socket.emit("messageFromServer", `Sum of ${a} and ${b} is ${sum}`);
+      socket.emit("messageFromServer", `difference of ${a} and ${b} is ${difference}`);
+      socket.emit("messageFromServer", `produdct of ${a} and ${b} is ${product}`);
+      socket.emit("messageFromServer", `quotient of ${a} and ${b} is ${quotient}`);
+      socket.emit("messageFromServer","enter the numbers ")
+
   });  
 });
 
-function addNumbers(a, b) {
-  return a + b;
-}
-function subtractNumbers(a, b) {
-  return a - b;
-}
+    function addNumbers(a, b) {
+        return a + b;
+    }
+    
+    function subtractNumbers(a, b) {
+        return a - b;
+    }
 
-server.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+    function multiplyNumbers(a, b) {
+        return a * b;
+    }
+    
+    function divideNumbers(a, b) {
+        if(b === 0){
+            return 'Cannot divide by zero'
+        }
+        return a / b;
+    }
+
+
+    server.listen(port, () =>
+        console.log(`Calculator app listening on port ${port}!`)
+    );
