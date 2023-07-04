a33e6bbff4cf74d416cc4fd350cf27600147e768
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-04 01:16:45 -0700

    fix some bugs

--- Client.js
+++ Client.js
@@ -4,18 +4,22 @@ const serverURL = 'http://localhost:3000'
 const socket = socketIOClient(serverURL)
 
 socket.on('connect', () => {
-    console.log('connected to server')
-    sendMessage()
+     console.log('connected to server')
+     console.log('enter the numbers')
+     sendMessage()
+
 })
+
 
 socket.on('messageFromServer', (message) => {
     console.log(message)
+
 })
 
-function sendMessage(){
+async function sendMessage(){
+    return new Promise((resolve)=>{ 
     const readline = require('readline').createInterface({
         input: process.stdin,
-        output: process.stdout
     })
     readline.question('Enter the numbers to add: ', (numbers) => {
         
@@ -23,19 +27,23 @@ function sendMessage(){
         if(numbers === 'exit'){
             readline.close()
             socket.disconnect()
+            resolve()
             return
         }
         if(numArray.length !== 2 || isNaN(numArray[0]) || isNaN(numArray[1])){
             console.log('Invalid input')
             readline.close()
             sendMessage()
+            resolve()
             return
         }
         const a = parseInt(numArray[0])
         const b = parseInt(numArray[1])
         socket.emit('messageFromClient', a, b)
         sendMessage()
+        resolve()
         })
+})
 }
 
 socket.on('disconnect', () => {
